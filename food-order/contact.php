1195759b371e57a23aee0aec45bfee3b979d1cb2
Animesh Panda <132000706+ani107p@users.noreply.github.com>
2023-11-16 08:49:46 +0530

    Add files via upload

---
+++ food-order/contact.php
@@ -0,0 +1,10 @@
+<?php include('partials-front/menu.php'); ?>
+<section class="categories">
+        <div class="container">
+            <h2 class="text-center">Contact Us</h2>
+            <p class="text-center">Kalinga Nagar, Ghatikia, Bhubaneswar, Odisha 751003</p><br>
+            <p class="text-center">mob-+91-7064846640</p>
+            <br>
+            <img src="images/capture.png" alt="location">
+            </div></section>
+<?php include('partials-front/footer.php'); ?>
